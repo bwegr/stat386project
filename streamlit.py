3d8ed2ba0ff644e630794acb97da6456e93999b2
Brandon Wegrowski <bwegrowski@gmail.com>
2023-12-18 18:06:44 -0700

    Uploading chans and users

--- streamlit.py
+++ streamlit.py
@@ -1,29 +1,64 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-import plotly.express as px
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+import ast
+from pandas import json_normalize
 
-st.title("BYU MBA Networks")
+# Assuming 'data' and 'users' are DataFrame available in your context
 
-url = "https://github.com/esnt/Data/raw/main/Names/popular_names.csv"
-df = pd.read_csv(url)
+# Sample data
+# data = pd.DataFrame(...)  # Your existing data
+# users = pd.DataFrame(...)  # Your existing users data
 
-selected_name = st.text_input('Enter Something', 'John')
+# Function definitions
+def num_and_sum(s):
+    pattern = r"'count': (\d+)"
+    counts = [int(num) for num in re.findall(pattern, s)]
+    return sum(counts)
 
-name_df = df[df['name'] == selected_name]
-if name_df.empty:
-    st.write('Name Not Found')
-else: 
-    fig = px.line(name_df, x = 'year', y = 'n', color = 'sex',
-                    color_discrete_sequence=px.colors.qualitative.Set2)
-    st. plotly_chart(fig)
+def convert_string_to_dict(s):
+    data = ast.literal_eval(s)
+    result = {'name': [], 'users': [], 'count': []}
+    for item in data:
+        result['name'].extend([item['name']] * item['count'])
+        result['users'].extend(item['users'])
+        result['count'].extend([item['count']] * item['count'])
+    return result
 
-selected_year = st.selectbox('Select a Year', df['year'].unique())
+# Streamlit App
+st.title("Top Reactors Visualization")
 
-year_df = df[df['year'] == selected_year]
-girl_names = year_df[year_df['sex'] == 'F'].sort_values(by='n', ascending=False).head(5)['name'].reset_index(drop=True)
-boy_names = year_df[year_df['sex'] == 'M'].sort_values(by='n', ascending=False).head(5)['name'].reset_index(drop=True)
-top_names = pd.concat([girl_names, boy_names], axis=1)
-top_names.columns = ['Female','Male']
-st.write(f"Top Names in {selected_year}")
-st.dataframe(top_names)
+# User input for the number of top users
+num_users = st.slider("Select the number of top users to display", 1, 20, 10)
+
+# Processing
+react1 = pd.DataFrame(data[data['reactions'].notna()][['user', 'reactions']])
+react1['sum_counts'] = react1['reactions'].astype(str).apply(num_and_sum)
+react1a = react1.groupby('user')['sum_counts'].sum().reset_index()
+
+list_of_dic = react1['reactions'].astype(str).apply(convert_string_to_dict).tolist()
+react2 = pd.DataFrame({'name': [], 'users': [], 'count': []})
+for i in list_of_dic:
+    max_length = max(len(lst) for lst in i.values())
+    for key in i:
+        i[key].extend([None] * (max_length - len(i[key])))
+    df = pd.DataFrame(i)
+    react2 = pd.concat([react2, df], ignore_index=True)
+
+react2b = pd.merge(react2, users[['id', 'real_name']], left_on='users', right_on='id', how='left')
+
+# Visualization
+react2a = react2b[['real_name', 'count']].groupby('real_name').count().reset_index().sort_values(by='count', ascending=False)
+paired = sorted(zip(react2a['count'][0:num_users], react2a['real_name'][0:num_users]))
+count_s, users_s = zip(*paired)
+
+fig, ax = plt.subplots()
+ax.barh(users_s, count_s)
+for index, value in enumerate(count_s):
+    ax.text(value-12, index, str(value), va='center')
+plt.xlabel('Messages Reacted To')
+plt.ylabel('Name')
+plt.title('Top Reactors (Oct 2023)')
+
+st.pyplot(fig)
